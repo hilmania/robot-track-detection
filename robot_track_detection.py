03f27c50bb751c30a242bc92124a3f3df1b8f0da
Hilman Ibnu <hia.develop@gmail.com>
2022-01-18 13:16:04 +0700

    [update] integrate fuzzy logic for VL and VR

--- robot_track_detection.py
+++ robot_track_detection.py
@@ -3,6 +3,10 @@ import cv2
 import time
 import math
 from math import atan2, cos, sin, sqrt, pi, atan, degrees
+import matplotlib.pyplot as plt
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
 
 # Create the circle
 colour = (0, 0, 255)
@@ -26,6 +30,7 @@ trajectories = []
 frame_idx = 0
 
 pointsList = [(0,0)] * 3
+global_angle = 0
 CM_TO_PIXEL = 32.0 / 640
 
 
@@ -175,7 +180,147 @@ def getOrientation(pts, img):
     cv2.putText(img, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1, cv2.LINE_AA)
     cv2.putText(img, "Angle : " + str(-int(np.rad2deg(angle)) - 90) + " degree", (20, 120), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2 )
 
+    global_angle = angle
     return angle
+
+def fuzzy_robot(d, a):
+    distance = ctrl.Antecedent(np.arange(0, 150, 1), 'distance')
+    uni_angle = np.array([-180, -90, -30, 0, 30, 90, 180])
+    angle = ctrl.Antecedent(uni_angle, 'angle')
+
+    uni_velocity = np.array([0, 55, 105, 155, 205, 255])
+    velocity = ctrl.Consequent(uni_velocity, 'velocity')
+
+    distance['sangat_dekat'] = fuzz.trapmf(distance.universe, [0, 0, 25, 50])
+    distance['dekat'] = fuzz.trimf(distance.universe, [25, 50, 75])
+    distance['cukup'] = fuzz.trimf(distance.universe, [50, 75, 100])
+    distance['jauh'] = fuzz.trimf(distance.universe, [75, 100, 125])
+    distance['sangat_jauh'] = fuzz.trapmf(distance.universe, [100, 125, 150, 150])
+
+    # distance.view()
+    # plt.title("Distance Membership Function")
+
+    angle['sangat_kiri'] = fuzz.trapmf(angle.universe, [-180, -180, -90, -30])
+    angle['kiri'] = fuzz.trimf(angle.universe, [-90, -30, 0])
+    angle['lurus'] = fuzz.trimf(angle.universe, [-30, 0, 30])
+    angle['kanan'] = fuzz.trimf(angle.universe, [0, 30, 90])
+    angle['sangat_kanan'] = fuzz.trapmf(angle.universe, [30, 90, 180, 180])
+
+    # angle.view()
+    # plt.title("Angle Membership Function")
+
+    velocity['stop'] = fuzz.trapmf(velocity.universe, [0, 0, 55, 105])
+    velocity['very_slow'] = fuzz.trimf(velocity.universe, [55, 105, 155])
+    velocity['slow'] = fuzz.trimf(velocity.universe, [105, 155, 205])
+    velocity['normal'] = fuzz.trimf(velocity.universe, [155, 205, 255]) 
+    velocity['fast'] = fuzz.trimf(velocity.universe, [205, 255, 255])
+
+    # velocity.view()
+    # plt.title("Velocity Membership Function")
+
+    # rule_roda_kanan
+    rule1_roda_kanan = ctrl.Rule(distance['sangat_dekat'] | angle['sangat_kiri'], velocity['stop'])
+    rule2_roda_kanan = ctrl.Rule(distance['sangat_dekat'] | angle['kiri'], velocity['very_slow'])
+    rule3_roda_kanan = ctrl.Rule(distance['sangat_dekat'] | angle['lurus'], velocity['very_slow'])
+    rule4_roda_kanan = ctrl.Rule(distance['sangat_dekat'] | angle['kanan'], velocity['slow'])
+    rule5_roda_kanan = ctrl.Rule(distance['sangat_dekat'] | angle['sangat_kanan'], velocity['very_slow'])
+
+    rule6_roda_kanan = ctrl.Rule(distance['dekat'] | angle['sangat_kiri'], velocity['very_slow'])
+    rule7_roda_kanan = ctrl.Rule(distance['dekat'] | angle['kiri'], velocity['very_slow'])
+    rule8_roda_kanan = ctrl.Rule(distance['dekat'] | angle['lurus'], velocity['slow'])
+    rule9_roda_kanan = ctrl.Rule(distance['dekat'] | angle['kanan'], velocity['slow'])
+    rule10_roda_kanan = ctrl.Rule(distance['dekat'] | angle['sangat_kanan'], velocity['slow'])
+
+    rule11_roda_kanan = ctrl.Rule(distance['cukup'] | angle['sangat_kiri'], velocity['slow'])
+    rule12_roda_kanan = ctrl.Rule(distance['cukup'] | angle['kiri'], velocity['slow'])
+    rule13_roda_kanan = ctrl.Rule(distance['cukup'] | angle['lurus'], velocity['normal'])
+    rule14_roda_kanan = ctrl.Rule(distance['cukup'] | angle['kanan'], velocity['normal'])
+    rule15_roda_kanan = ctrl.Rule(distance['cukup'] | angle['sangat_kanan'], velocity['normal'])
+
+    rule16_roda_kanan = ctrl.Rule(distance['jauh'] | angle['sangat_kiri'], velocity['slow'])
+    rule17_roda_kanan = ctrl.Rule(distance['jauh'] | angle['kiri'], velocity['normal'])
+    rule18_roda_kanan = ctrl.Rule(distance['jauh'] | angle['lurus'], velocity['normal'])
+    rule19_roda_kanan = ctrl.Rule(distance['jauh'] | angle['kanan'], velocity['normal'])
+    rule20_roda_kanan = ctrl.Rule(distance['jauh'] | angle['sangat_kanan'], velocity['fast'])
+
+    rule21_roda_kanan = ctrl.Rule(distance['sangat_jauh'] | angle['sangat_kiri'], velocity['normal'])
+    rule22_roda_kanan = ctrl.Rule(distance['sangat_jauh'] | angle['kiri'], velocity['normal'])
+    rule23_roda_kanan = ctrl.Rule(distance['sangat_jauh'] | angle['lurus'], velocity['normal'])
+    rule24_roda_kanan = ctrl.Rule(distance['sangat_jauh'] | angle['kanan'], velocity['fast'])
+    rule25_roda_kanan = ctrl.Rule(distance['sangat_jauh'] | angle['sangat_kanan'], velocity['fast'])
+
+    # rule roda kiri
+    rule1_roda_kiri = ctrl.Rule(distance['sangat_dekat'] | angle['sangat_kiri'], velocity['very_slow'])
+    rule2_roda_kiri = ctrl.Rule(distance['sangat_dekat'] | angle['kiri'], velocity['slow'])
+    rule3_roda_kiri = ctrl.Rule(distance['sangat_dekat'] | angle['lurus'], velocity['slow'])
+    rule4_roda_kiri = ctrl.Rule(distance['sangat_dekat'] | angle['kanan'], velocity['very_slow'])
+    rule5_roda_kiri = ctrl.Rule(distance['sangat_dekat'] | angle['sangat_kanan'], velocity['stop'])
+
+    rule6_roda_kiri = ctrl.Rule(distance['dekat'] | angle['sangat_kiri'], velocity['slow'])
+    rule7_roda_kiri = ctrl.Rule(distance['dekat'] | angle['kiri'], velocity['slow'])
+    rule8_roda_kiri = ctrl.Rule(distance['dekat'] | angle['lurus'], velocity['normal'])
+    rule9_roda_kiri = ctrl.Rule(distance['dekat'] | angle['kanan'], velocity['very_slow'])
+    rule10_roda_kiri = ctrl.Rule(distance['dekat'] | angle['sangat_kanan'], velocity['very_slow'])
+
+    rule11_roda_kiri = ctrl.Rule(distance['cukup'] | angle['sangat_kiri'], velocity['slow'])
+    rule12_roda_kiri = ctrl.Rule(distance['cukup'] | angle['kiri'], velocity['normal'])
+    rule13_roda_kiri = ctrl.Rule(distance['cukup'] | angle['lurus'], velocity['normal'])
+    rule14_roda_kiri = ctrl.Rule(distance['cukup'] | angle['kanan'], velocity['slow'])
+    rule15_roda_kiri = ctrl.Rule(distance['cukup'] | angle['sangat_kanan'], velocity['slow'])
+
+    rule16_roda_kiri = ctrl.Rule(distance['jauh'] | angle['sangat_kiri'], velocity['slow'])
+    rule17_roda_kiri = ctrl.Rule(distance['jauh'] | angle['kiri'], velocity['normal'])
+    rule18_roda_kiri = ctrl.Rule(distance['jauh'] | angle['lurus'], velocity['slow'])
+    rule19_roda_kiri = ctrl.Rule(distance['jauh'] | angle['kanan'], velocity['slow'])
+    rule20_roda_kiri = ctrl.Rule(distance['jauh'] | angle['sangat_kanan'], velocity['normal'])
+
+    rule21_roda_kiri = ctrl.Rule(distance['sangat_jauh'] | angle['sangat_kiri'], velocity['normal'])
+    rule22_roda_kiri = ctrl.Rule(distance['sangat_jauh'] | angle['kiri'], velocity['normal'])
+    rule23_roda_kiri = ctrl.Rule(distance['sangat_jauh'] | angle['lurus'], velocity['slow'])
+    rule24_roda_kiri = ctrl.Rule(distance['sangat_jauh'] | angle['kanan'], velocity['normal'])
+    rule25_roda_kiri = ctrl.Rule(distance['sangat_jauh'] | angle['sangat_kanan'], velocity['fast'])
+
+    # rule_roda_kiri
+    robot_ctrl_right = ctrl.ControlSystem(
+        [rule1_roda_kanan, rule2_roda_kanan, rule3_roda_kanan, rule4_roda_kanan, rule5_roda_kanan,
+        rule6_roda_kanan, rule7_roda_kanan, rule8_roda_kanan, rule9_roda_kanan, rule10_roda_kanan,
+        rule11_roda_kanan, rule12_roda_kanan, rule13_roda_kanan, rule14_roda_kanan, rule15_roda_kanan,
+        rule16_roda_kanan, rule17_roda_kanan, rule18_roda_kanan, rule19_roda_kanan, rule20_roda_kanan,
+        rule21_roda_kanan, rule22_roda_kanan, rule23_roda_kanan, rule24_roda_kanan, rule25_roda_kanan]
+    )
+
+    robot_ctrl_left = ctrl.ControlSystem(
+        [rule1_roda_kiri, rule2_roda_kiri, rule3_roda_kiri, rule4_roda_kiri, rule5_roda_kiri,
+        rule6_roda_kiri, rule7_roda_kiri, rule8_roda_kiri, rule9_roda_kiri, rule10_roda_kiri,
+        rule11_roda_kiri, rule12_roda_kiri, rule13_roda_kiri, rule14_roda_kiri, rule15_roda_kiri,
+        rule16_roda_kiri, rule17_roda_kiri, rule18_roda_kiri, rule19_roda_kiri, rule20_roda_kiri,
+        rule21_roda_kiri, rule22_roda_kiri, rule23_roda_kiri, rule24_roda_kiri, rule25_roda_kiri]
+    )
+
+    wheel_right = ctrl.ControlSystemSimulation(robot_ctrl_right)
+    wheel_left = ctrl.ControlSystemSimulation(robot_ctrl_left)
+
+    wheel_right.input['distance'] = d
+    wheel_right.input['angle'] = a
+
+    wheel_left.input['distance'] = d
+    wheel_left.input['angle'] = a
+
+    wheel_right.compute()
+    wheel_left.compute()
+
+    print("velocity_right_wheel : ", wheel_right.output['velocity'])
+    print("velocity_left_wheel : ", wheel_left.output['velocity'])
+    # velocity.view(sim=wheel_right)
+    # plt.title("Velocity Right Wheel")
+    # velocity.view(sim=wheel_left)
+    # plt.title("Velocity Left Wheel")
+
+    # plt.show()
+    vel = []
+    vel.append(wheel_right.output['velocity'])
+    vel.append(wheel_left.output['velocity'])
+    return vel
 
 
 # ======================================================================================================================
@@ -253,8 +398,8 @@ while True:
         cv2.putText(img, "Coordinate : " + str(trajectories[0][0]), (20, 90), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2 )
         # print(trajectories[0][0][1])
         print(pointsList[1])
-        if pointsList is not None:
-            cv2.putText(img, "Distance : " + str( distance(pointsList[1][0], pointsList[1][1], trajectories[0][0][0], trajectories[0][0][1]) ) + " cm", (20, 150), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        # if pointsList is not None:
+        cv2.putText(img, "Distance : " + str( distance(pointsList[1][0], pointsList[1][1], trajectories[0][0][0], trajectories[0][0][1]) ) + " cm", (20, 150), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
         # drawAxis(img, pointsList[0], pointsList[1], (255, 255, 0), 1)
 
     # Update interval - When to update and detect new features
@@ -283,8 +428,13 @@ while True:
     # calculate the FPS for current frame detection
     fps = 1 / (end - start)
 
+    velo = fuzzy_robot(distance(pointsList[1][0], pointsList[1][1], trajectories[0][0][0], trajectories[0][0][1]), global_angle )
+    print(velo)
     # Show Results
     cv2.putText(img, f"{fps:.2f} FPS", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(img, "VR : " + str(round(velo[0], 2)) + " PWM", (20, 180), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(img, "VL : " + str(round(velo[1], 2)) + " PWM", (20, 210), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
 
     cv2.imshow('Optical Flow', img)
     cv2.imshow('Mask', mask)
